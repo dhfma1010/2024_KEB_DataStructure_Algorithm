294e7cca1338f300dde2603b1b52b0782d70d040
dhfma1010 <dhfma1010@gmail.com>
2024-02-08 15:43:39 +0900

    v1.2 poly example2

--- day001.py
+++ day001.py
@@ -1,42 +1,38 @@
-def print_poly(f_x) -> str:
-    term = len(f_x) - 1
+def print_poly(t_x, f_x) -> str:
+
     poly_expression = "f(x) = "
 
     for i in range(len(fx)):
-
+        term = t_x[i]
         coefficient = f_x[i]
 
-        if coefficient == 0:  # 계수가 0이면 더하지 않고 넘어감
-            term -= 1
-            continue
         if coefficient >= 0 and i != 0:  # 맨 앞에는 +부호 붙이지 않음
             poly_expression += "+"
         poly_expression += f'{coefficient}X^{term} '
-        term -= 1
 
     return poly_expression
 
 
-def calculation_poly(x_value, f_x) -> int:
+def calculation_poly(x_value, t_x, f_x) -> int:
     return_value = 0
-    term = len(f_x) - 1
+
 
     for i in range(len(fx)):
+        term = tx[i]
         coefficient = f_x[i]
         return_value += coefficient * pow(x_value, term)
-        term -= 1
+
 
     return return_value
 
-
-
-fx = [2, 3, 4, 0, -9]
+fx = [2, 3, 4]
+tx = [500, 10, 0]
 
 if __name__ == "__main__":
 
-    print(print_poly(fx))
+    print(print_poly(tx, fx))
 
-    pxValue = calculation_poly(int(input("X 값 : ")), fx)
+    pxValue = calculation_poly(int(input("X 값 : ")), tx, fx)
     print(pxValue)
 
 
