c945c55efe539218bf47ebc96bb3e7683da7ff05
dhfma1010 <dhfma1010@gmail.com>
2024-02-14 14:34:45 +0900

    v1.5 graph dfs

--- day002.py
+++ day002.py
@@ -5,23 +5,16 @@ def dfs(g, i, visited):
         if g[i][j] == 1 and not visited[j]:
             dfs(g, j, visited)
 
-'''
-G1 = Graph(5)
-G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-G1.graph[4][2] = 1; G1.graph[4][3] = 1
-'''
 
 graph = [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 1, 0],
-    [0, 1, 0, 0, 1],
-    [1, 1, 0, 0, 1],
-    [0, 0, 1, 1, 0]
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0]
 ]
 
-
-visited = [0] * len(graph)
+visited = [0 for _ in range(len(graph))]
+# visited = [0] * len(graph)
 dfs(graph, 0, visited)
